7ec9aa40a5d85ec96ba2dce8ef7df3952b640fdd
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-03-27 01:02:23 -0400

    initial folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './scenes/chat/chat.component';
+import { HomePageComponent } from './scenes/home-page/home-page.component';
+import { LoginPageComponent } from './scenes/login-page/login-page.component';
+import { NavbarComponent } from './scenes/navbar/navbar.component';
+import { ProfilePageComponent } from './scenes/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    NavbarComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
